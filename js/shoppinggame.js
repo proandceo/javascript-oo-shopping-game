e29ceac14c7aeaff54d68513fe49da5eb38f1876
Rex <proandceo@gmail.com>
2023-04-19 14:30:54 +1000

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -44,12 +44,35 @@ Object.defineProperty(Product.prototype, "daysToExpire", {
 });
 
 // Add method getDetails to Product here
+Product.prototype.getDetails = function () {
+  return `Product Name: ${this.name} , Product Price: ${this.price}`;
+};
 
 // Define the MagicProduct class here
+function MagicProduct(id, name, price, expiryDate, points, isBonus) {
+  Product.call(this, id, name, price, expiryDate);
+  this.points = points;
+  this.isBonus = isBonus;
+}
 
 // Establish inheritance between Product() & MagicProduct() here
+MagicProduct.prototype = Object.create(Product.prototype);
 
 // Define Rating class here
+class Rating {
+  constructor() {
+    this.rate = "";
+  }
+  set rating(value) {
+    if (value > 1 && value <= 4) {
+      rate = "OK";
+    } else if (value >= 5 && value <= 7) {
+      rate = "GOOD";
+    } else if (value > 7) {
+      rate = "EXCEPTIONAL";
+    } else rate = "BAD";
+  }
+}
 
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
