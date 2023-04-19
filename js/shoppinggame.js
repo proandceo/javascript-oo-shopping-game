db5860e8e7d8141116b8e1876b5135e5276c174d
Rex <proandceo@gmail.com>
2023-04-19 15:33:17 +1000

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -286,16 +286,20 @@ const findPointsToBill = (roundedTotal) => {
 
 // Complete this function
 const findPointsForExpDate = (prod) => {
-  if (prod.expiryDate < 30) {
-    return 10;
-  } else {
-    return 0;
-  }
+  return prod.expiryDate < 30 ? 10 : 0;
 };
 
 const calculatePoints = (prod, tBill) => {
   let pointsToBill = findPointsToBill(Math.round(tBill));
   let pointsForExpDate = findPointsForExpDate(prod);
+  player.score = player.score + pointsToBill + pointsForExpDate;
+  if (prod instanceof MagicProduct) {
+    if (prod.isBonus) {
+      player.addPoints(prod.points);
+    } else {
+      player.deductPoints(prod.points);
+    }
+  }
 };
 
 // Complete this function
