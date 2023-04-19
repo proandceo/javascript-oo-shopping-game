0b0a59d1fa2b6ef23f31e6707aa334cc66c838b3
Rex <proandceo@gmail.com>
2023-04-19 15:19:52 +1000

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -247,9 +247,9 @@ function loadMasterData() {
 
 // Complete this function
 const findProductById = (id) => {
-  if (id == this.id) {
-    return new Product();
-  }
+  return function (product) {
+    return product.id == id;
+  };
 };
 
 // Complete this function
